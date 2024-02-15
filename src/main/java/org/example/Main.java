cd0d336239001feeb3b2134fa2cab99156a9d9fa
alishershayakhmetov <shayakhmetovalisher1@gmail.com>
2024-02-15 13:36:25 +0600

    Refactoring the code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,10 @@
 package org.example;
 
 import org.example.data.PostgresDB;
-import org.example.handlers.HandleIOStream;
 import java.sql.*;
 
 
-public class Main extends HandleIOStream{
+public class Main{
     public static void main(String[] args)  {
         Connection con = PostgresDB.GetConnect();
         Auth auth = new Auth(con);
